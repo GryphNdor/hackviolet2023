fdb0606bc82cec792ac923cae8aafbe2473ea1c3
Brian Lee <75905365+btlee264855@users.noreply.github.com>
2023-02-04 16:39:05 -0500

    linus

--- backend/image_recognition.py
+++ backend/image_recognition.py
@@ -2,11 +2,11 @@ import cv2
 import numpy as np
 import face_recognition
 
+debug = False
+
 # load real image
 img_bgr = face_recognition.load_image_file('real_linus.jpg')
 img_rgb = cv2.cvtColor(img_bgr,cv2.COLOR_BGR2RGB)
-cv2.imshow('bgr', img_bgr)
-cv2.imshow('rgb', img_rgb)
 cv2.waitKey
 
 # color map
@@ -17,9 +17,9 @@ face = face_recognition.face_locations(img_modi_rgb)[0]
 copy = img_modi_rgb.copy()
 # ------ Drawing bounding boxes around Faces------------------------
 cv2.rectangle(copy, (face[3], face[0]),(face[1], face[2]), (255,0,255), 2)
-cv2.imshow('copy', copy)
-cv2.imshow('MODI',img_modi_rgb)
-cv2.waitKey(0)
+if(debug):
+    cv2.imshow('Original_Detected', copy)
+cv2.waitKey
 
 img_modi = face_recognition.load_image_file('real_linus.jpg')
 img_modi = cv2.cvtColor(img_modi,cv2.COLOR_BGR2RGB)
@@ -34,7 +34,12 @@ train_encode = face_recognition.face_encodings(img_modi)[0]
 test = face_recognition.load_image_file('fake_linus.jpg')
 test = cv2.cvtColor(test, cv2.COLOR_BGR2RGB)
 test_encode = face_recognition.face_encodings(test)[0]
-print(face_recognition.compare_faces([train_encode],test_encode))
+found = face_recognition.compare_faces([train_encode],test_encode)
+if(found):
+    print("the image has been found")
+else:
+    print("the image has not been found")
 cv2.rectangle(img_modi, (face[3], face[0]),(face[1], face[2]), (255,0,255), 1)
-cv2.imshow('img_modi', img_modi)
+if(debug):
+    cv2.imshow('Fake', test)
 cv2.waitKey(0)
